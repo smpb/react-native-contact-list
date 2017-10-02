660d63acb708a6bcd56bac87b33aa015331bc258
Sérgio Bernardino <me@sergiobernardino.net>
2017-10-02 23:01:51 +0100

    Lists are scrollable and sorted; re-added tab Headers

--- App.js
+++ App.js
@@ -5,10 +5,11 @@ import React from 'react';
 import {
   Text,
   View,
+  ScrollView,
   StatusBar,
   Platform,
 } from 'react-native';
-import { List, ListItem, Icon } from 'react-native-elements'
+import { Header, List, ListItem, Icon } from 'react-native-elements'
 import { TabNavigator } from 'react-navigation';
 
 let contactList = [
@@ -19,12 +20,18 @@ let contactList = [
   { name: 'Anabela',  key: '629452367', favorite: false },
   { name: 'Carlos',   key: '822372354', favorite: false },
   { name: 'Amélie',   key: '483001478', favorite: true  },
+  { name: 'Elias',    key: '893465482', favorite: false },
+  { name: 'Duarte',   key: '907476232', favorite: false },
+  { name: 'Isabel',   key: '782534741', favorite: false },
   { name: 'Elisa',    key: '119710387', favorite: false },
   { name: 'José',     key: '087190363', favorite: false },
+  { name: 'Madalena', key: '279727203', favorite: false },
 ];
 
 const ACTIVE_TAB_COLOR = '#007aff';
 const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+
+var _sort = (a, b) => { return (a.name > b.name) ? 1 : ((a.name < b.name) ? -1 : 0); };
 
 class ContactsScreen extends React.Component {
   static navigationOptions = {
@@ -40,18 +47,24 @@ class ContactsScreen extends React.Component {
 
   render() {
     return (
-      <List containerStyle={{marginBottom: 20}}>
-        {
-          contactList.map((l, i) => (
-            <ListItem
-              key={i}
-              title={l.name}
-              subtitle={l.key}
-              leftIcon={{name: 'person'}}
-            />
-          ))
-        }
-      </List>
+      <ScrollView>
+        <Header
+          centerComponent={{ text: 'CONTACTS', style: { fontWeight: 'bold', color: '#000' } }}
+          backgroundColor='#fff'
+        />
+        <List containerStyle={{marginTop: 70}}>
+          {
+            contactList.sort(_sort).map((l, i) => (
+              <ListItem
+                key={i}
+                title={l.name}
+                subtitle={l.key}
+                leftIcon={{name: 'person'}}
+              />
+            ))
+          }
+        </List>
+      </ScrollView>
     );
   }
 }
@@ -70,18 +83,24 @@ class FavoritesScreen extends React.Component {
 
   render() {
     return (
-      <List containerStyle={{marginBottom: 20}}>
-        {
-          contactList.filter(l => l.favorite ).map((l, i) => (
-            <ListItem
-              key={i}
-              title={l.name}
-              subtitle={l.key}
-              leftIcon={{name: 'person'}}
-            />
-          ))
-        }
-      </List>
+      <ScrollView>
+        <Header
+          centerComponent={{ text: 'FAVORITES', style: { fontWeight: 'bold', color: '#000' } }}
+          backgroundColor='#fff'
+        />
+        <List containerStyle={{marginTop: 70}}>
+          {
+            contactList.sort().filter(l => l.favorite).map((l, i) => (
+              <ListItem
+                key={i}
+                title={l.name}
+                subtitle={l.key}
+                leftIcon={{name: 'person'}}
+              />
+            ))
+          }
+        </List>
+      </ScrollView>
     );
   }
 }
@@ -107,10 +126,10 @@ const Nav = TabNavigator(
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{ flex : 1, paddingTop: STATUSBAR_HEIGHT, }}>
-        <StatusBar translucent={false} barStyle="dark-content" />
+      <View style={{ flex : 1 }}>
         <Nav />
       </View>
     );
   }
 }
+
